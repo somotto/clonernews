62b8efcfee14b2aa8f57d7b798b8f111b9d7f0b3
krodgers <kaunda.rodgers@gmail.com>
2024-10-20 16:12:22 +0300

    feat: update with throttle func

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const apiBase = 'https://hacker-news.firebaseio.com/v0';
+let currentIndex = 0;
+const postsPerPage = 10;
+const postTypes = ['topstories', 'newstories', 'jobstories', 'askstories', 'showstories'];
+let currentType = 'topstories';
+
+function throttle(func, limit) {
+    let inThrottle;
+    return function () {
+        const args = arguments;
+        const context = this;
+        if (!inThrottle) {
+            func.apply(context, args);
+            inThrottle = true;
+            setTimeout(() => inThrottle = false, limit);
+        }
+    }
+}
 function showLoading(elementId) {
     const element = document.getElementById(elementId);
     element.innerHTML = '<div class="loading">Loading...</div>';
